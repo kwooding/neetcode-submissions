c33f8ff71c8ef3757acb2ca2ee01382c71f3fdcc
Keegan Wooding <157065243+kwooding@users.noreply.github.com>
2026-08-14 10:59:36 -0400

    Add: minimize-the-maximum-difference-of-pairs - submission-0

---
+++ Data Structures & Algorithms/minimize-the-maximum-difference-of-pairs/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        nums.sort()
+        lo,hi = 0 , nums[-1] - nums[0]
+        def countPairs(d):
+            count, i = 0,0
+            while i < len(nums) - 1:
+                if nums[i+1] - nums[i] <= d:
+                    count += 1
+                    i += 2
+                else:
+                    i +=1
+            
+            return count
+
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if countPairs(mid) >= p:
+                hi = mid
+            else:
+                lo = mid + 1
+
+
+        return lo
